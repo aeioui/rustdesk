5ce539129c359efb095412b48b9a1d7b8ef9ab9c
aeioui <aeioui@163.com>
2025-09-03 21:35:39 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -993,7 +993,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://www.20031003.xyz".to_owned()
 }
 
 #[inline]
